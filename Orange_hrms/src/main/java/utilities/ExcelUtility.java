188ac872203ea84b5878be995ca27a5677eec59e
Thiyaka Priyan <Thiyaka Priyan@LAPTOP-CFHD6V84>
2025-08-04 22:25:31 +0530

    created ExcelUtility file

---
+++ Orange_hrms/src/main/java/utilities/ExcelUtility.java
@@ -0,0 +1,85 @@
+package utilities;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import javafx.scene.input.DataFormat;
+
+public class ExcelUtility {
+
+	public FileInputStream fi;
+	public FileOutputStream fo;
+	public XSSFWorkbook workbook;
+	public XSSFSheet sheet;
+	public XSSFRow row;
+	public XSSFCell cell;
+	public XSSFCellStyle style;
+	String path;
+
+	public ExcelUtility(String path) {
+		this.path = path;
+	}
+
+	public int getRowCount(String sheetName) throws IOException {
+
+		fi = new FileInputStream(path);
+		workbook = new XSSFWorkbook(fi);
+		sheet = workbook.getSheet(sheetName);
+		int rowcount = sheet.getLastRowNum();
+		workbook.close();
+		fi.close();
+
+		return rowcount;
+
+	}
+
+	public int getCellCount(String sheetName, int rownum) throws IOException {
+
+		fi = new FileInputStream(path);
+		workbook = new XSSFWorkbook(fi);
+		sheet = workbook.getSheet(sheetName);
+		row = sheet.getRow(rownum);
+		int cellcount = row.getLastCellNum();
+		workbook.close();
+		fi.close();
+
+		return cellcount;
+
+	}
+
+	public String getCellData(String sheetName, int rownum, int colnum) throws IOException {
+
+		fi = new FileInputStream(path);
+		workbook = new XSSFWorkbook(fi);
+		sheet = workbook.getSheet(sheetName);
+		row = sheet.getRow(rownum);
+		cell = row.getCell(colnum);
+
+		DataFormatter formatter = new DataFormatter();
+		String data;
+
+		try {
+			data = formatter.formatCellValue(cell);// Returns the formatted value of a cell as a String regardless of
+			// the cell type.
+
+		} catch (Exception e) {
+
+			data = "";
+		}
+
+		workbook.close();
+		fi.close();
+
+		return data;
+
+	}
+
+}
