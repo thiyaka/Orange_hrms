84915618f07c248de7ccfc451aad19c015f36e63
Thiyaka Priyan <Thiyaka Priyan@LAPTOP-CFHD6V84>
2025-08-06 19:13:54 +0530

    updated TakeScreenShot method

--- Orange_hrms/src/main/java/base/BaseTest.java
+++ Orange_hrms/src/main/java/base/BaseTest.java
@@ -1,11 +1,16 @@
 package base;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 import java.util.Properties;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -13,21 +18,23 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 
+
+
 public class BaseTest {
 
 	public static WebDriver driver;
 	public Properties p;
-	
+
 	@BeforeClass
-	@Parameters({"os","browser"})
+	@Parameters({ "os", "browser" })
 	public void setup(String os, String br) {
-		
+
 		try {
-			FileReader file= new FileReader("./src/test/resources/config.properties");
-			
-			p= new Properties();
+			FileReader file = new FileReader("./src/test/resources/config.properties");
+
+			p = new Properties();
 			p.load(file);
-			
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -35,30 +42,52 @@ public class BaseTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		if(p.getProperty("execution_env").equalsIgnoreCase("local")) {
-			switch(br.toLowerCase()) {
-			case "chrome":driver= new ChromeDriver();break;
-			case "edge": driver= new EdgeDriver();break;
-			default: System.out.println("Invalid browser ");break;
+
+		if (p.getProperty("execution_env").equalsIgnoreCase("local")) {
+			switch (br.toLowerCase()) {
+			case "chrome":
+				driver = new ChromeDriver();
+				break;
+			case "edge":
+				driver = new EdgeDriver();
+				break;
+			default:
+				System.out.println("Invalid browser ");
+				break;
 			}
 		}
-		
-		
+
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
 		driver.get(p.getProperty("appURL1"));
 		driver.manage().window().maximize();
-		
+
 	}
-	
+
 	@AfterClass
 	public void teardown() {
-		
-		//driver.close();	
+
+		// driver.close();
+
 	}
 	
-	
+	public  String captureScreenShot(String name) {
+		
+		String timeStamp= new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		
+		TakesScreenshot screenshot= (TakesScreenshot)driver;
+		File srcfile= screenshot.getScreenshotAs(OutputType.FILE);
+		
+		String targetFilePath= System.getProperty("user.dir")+"\\screenshots\\"+  name + "_"+ timeStamp + ".png";
+		
+		File targetFile= new File(targetFilePath);
+		srcfile.renameTo(targetFile);
+		
+		return targetFilePath;
+		
+		
+
+	}
+
 }
