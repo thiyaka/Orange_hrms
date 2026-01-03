c26fb93db5b980d25c38976cb086dcafaf76c082
Thiyaka Priyan <Thiyaka Priyan@LAPTOP-CFHD6V84>
2026-01-03 16:16:31 +0530

    updated Config properties and reusable Explicit Wait mechanism

--- Orange_hrms/src/main/java/utilities/ReusableUtilities.java
+++ Orange_hrms/src/main/java/utilities/ReusableUtilities.java
@@ -17,21 +17,26 @@ public class ReusableUtilities {
 		element.sendKeys(value);
 	}
 
-	public static WebElement waitForElementToBeClickable(WebDriver driver, WebElement element, int timeoutInSeconds) {
+	public static WebElement waitForElementToBeClickable(WebDriver driver, WebElement element, int timeout) {
 
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
+		// WebDriverWait wait = new WebDriverWait(driver,
+		// Duration.ofSeconds(timeoutInSeconds));
+		WebDriverWait wait = waitinitializationCustomTimer(driver, timeout);
 		return wait.until(ExpectedConditions.elementToBeClickable(element));
 	}
 
-	public static WebElement waitForElementVisiblity(WebDriver driver, WebElement element, int timeoutInSeconds) {
+	public static WebElement waitForElementVisiblity(WebDriver driver, WebElement element, int timeout) {
 
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
+		// WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
+		WebDriverWait wait = waitinitializationCustomTimer(driver, timeout);
 		return wait.until(ExpectedConditions.visibilityOf(element));
 	}
 
 	public static WebElement waitAndClear(WebDriver driver, WebElement element, int timeout) {
 
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeout));
+		// WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeout));
+
+		WebDriverWait wait = waitinitializationCustomTimer(driver, timeout);
 		WebElement el = wait.until(ExpectedConditions.elementToBeClickable(element));
 		el.clear();
 		wait.until(ExpectedConditions.attributeToBe(el, "value", ""));
@@ -50,14 +55,28 @@ public class ReusableUtilities {
 
 	public static boolean waitForSuccessmsg(WebDriver driver, WebElement toast, int timeout) {
 
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeout));
+		// WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeout));
+
+		WebDriverWait wait = waitinitializationCustomTimer(driver, timeout);
 		return wait.until(ExpectedConditions.visibilityOf(toast)).isDisplayed();
 	}
-	
+
 	public static Boolean waitForFormToLoad(WebDriver driver, WebElement formLoader) {
-	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-	    return wait.until(ExpectedConditions.invisibilityOf(formLoader));
+		// WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebDriverWait wait = waitinitializationDefaultTimer(driver);
+		return wait.until(ExpectedConditions.invisibilityOf(formLoader));
 	}
 
+	// wait method
+
+	public static WebDriverWait waitinitializationDefaultTimer(WebDriver driver) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		return wait;
+	}
+	
+	public static WebDriverWait waitinitializationCustomTimer(WebDriver driver, int timeout) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		return wait;
+	}
 
 }
